e7d7bb6e5433f3dba7d252c901f2bbb9917f90b3
GitHub Action <action@github.com>
2022-06-23 14:36:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/lzj960515/kquniversalgenerator/MyBundle.kt
+++ src/main/kotlin/com/github/lzj960515/kquniversalgenerator/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.lzj960515.kquniversalgenerator
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/lzj960515/kquniversalgenerator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.lzj960515.kquniversalgenerator.services
+
+import com.github.lzj960515.kquniversalgenerator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
